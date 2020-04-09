57432d803727c78286b988ea31b1b1dfe1783ea5
isv_吕进浩 <hz936302257@163.com>
2020-04-09 19:36:24 +0800

    feat:"登录注册"

---
+++ src/components/log/log-main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="log-main">
+    <LoginTitle :title="title" />
+  </div>
+</template>
+<script>
+import LoginTitle from "../../components/components/login/login-title";
+export default {
+  components: {
+    LoginTitle
+  },
+  data() {
+    return {
+      title: "使用手机登录"
+    };
+  }
+};
+</script>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <LoginTitle />
+    <LoginMain />
   </div>
 </template>
 <script>
 import LoginTitle from '../../components/log/log-title'
+import LoginMain from '../../components/log/log-main'
 export default {
   components:{
-    LoginTitle
+    LoginTitle,
+    LoginMain
   }
 
 };
