62a83d4c172a5de594f50b8e11b0dfed9e63eaf7
fengfengying <2119179562@qq.com>
2020-04-30 17:06:11 +0800

    样式修改及数据加载0430

--- src/components/home/components/recommend/recommend-card.vue
+++ src/components/home/components/recommend/recommend-card.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="overlay-card">
     <van-card
-      price="2.00"
-      origin-price="10.00"
-      desc="B01（升级）"
-      title="【sum37]=】 呼吸苏秘37度 经典惊喜水分气垫CC霜定妆"
-      thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
-      v-for="i in 4"
-      :key="i"
-      @click="onCard(i)"
+      v-for="(item,index) in dataList"
+      :key="index"
+      :price="item.vip_price"
+      :desc="item.describes"
+      :title="'【'+item.p_name+'】'+item.p_title"
+      :thumb="item.picture"
+      @click="onCard(index)"
     >
       <template #footer>
-        <p>2220条评论</p>
+        <p v-text="item.diss_num+'条评论'"></p>
       </template>
     </van-card>
   </div>
@@ -32,13 +31,11 @@ export default {
     //获取商品信息
     async getData() {
       let type = this.selectID;
-      window.console.log(type);
       let res = await appSelect(type).then();
       try {
         if (res.code == 0) {
           this.dataList = res.payload;
-        } else {
-          // this.toPopUpWindows(res.msg);
+          window.console.log(this.dataList);
         }
       } catch (error) {
         window.console.log(error);
@@ -62,27 +59,25 @@ export default {
     position: relative;
     margin-bottom: 10px;
     padding: 8px 16px 8px 0;
-    height: 165px;
+    // height: 165px;
     background-color: #fff;
     .van-card__header {
       width: 100%;
       height: 100%;
       .van-card__thumb {
-        width: 155px;
-        height: 150px;
         padding-left: 10px;
       }
       .van-card__content {
-        font-size: 13px;
+        font-size: 10px;
         color: #333;
         div {
-          height: 50px;
           .van-card__title {
             line-height: 16px;
             font-size: 16px;
           }
           .van-card__desc {
-            line-height: 30px;
+            line-height: 25px;
+            margin-left: 10px;
           }
         }
 
